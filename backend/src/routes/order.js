095e005fc06b1d8b2ebe082890ad38c318901215
mohamed <mohammed2571994@gmail.com>
2020-12-27 20:21:52 +0200

    Add get all orders route

--- backend/src/routes/order.js
+++ backend/src/routes/order.js
@@ -3,11 +3,19 @@ require('express-async-errors');
 const express = require('express');
 const router = express.Router();
 const orderController = require('../controllers/order');
-const { isAuthenticated } = require('../middleware/authMiddleware');
+const { isAuthenticated, isAdmin } = require('../middleware/authMiddleware');
 
-router.route('/').post(isAuthenticated, orderController.createOrder);
+router
+  .route('/')
+  .get(isAuthenticated, isAdmin, orderController.getAllOrders)
+  .post(isAuthenticated, orderController.createOrder);
+
 router.route('/me').get(isAuthenticated, orderController.getLoggedInUserOrders);
-router.route('/:id').get(isAuthenticated, orderController.getOrderDetails);
+
+router
+  .route('/:id')
+  .get(isAuthenticated, isAdmin, orderController.getOrderDetails);
+
 router.route('/:id/pay').patch(isAuthenticated, orderController.payOrder);
 
 module.exports = router;
